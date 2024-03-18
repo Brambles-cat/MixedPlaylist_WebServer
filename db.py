23743fa9cc7b2dd22565b71d90709d04b51de959
Brambles-cat <gael.alejos05@gmail.com>
2024-03-17 22:08:04 -0700

    Added update_data method and other small changes

--- db.py
+++ db.py
@@ -9,11 +9,11 @@ def create_table(conn: Connection, create_table_sql: str):
     except Error as e:
         print(e)
 
-def insert_data(data: tuple, keepConnectionOpen= False, connection=None) -> Connection | None:
+def initialize_playlist(data: tuple, keepConnectionOpen=False, connection=None) -> Connection | None:
     conn: Connection = sqlite3.connect("saved_playlists.db", check_same_thread=False) if connection is None else connection
     
-    sql = ''' INSERT INTO test(id,owner_id,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10)
-              VALUES(?,?,?,?,?,?,?,?,?,?,?,?) '''
+    sql = ''' INSERT INTO test(id,owner_id,v1)
+              VALUES(?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, data)
     conn.commit()
@@ -23,12 +23,25 @@ def insert_data(data: tuple, keepConnectionOpen= False, connection=None) -> Conn
     
     conn.close()
 
+def update_data(playlist_id: str, vid_data: str, index: int): # super susceptible to sql injection and stuff. CHANGE LATER
+
+    sql = f'UPDATE test SET v{index} = ? WHERE id = ? LIMIT 1'
+
+    conn: Connection = sqlite3.connect("saved_playlists.db", check_same_thread=False)
+    cur = conn.cursor()
+    cur.execute(sql, (vid_data, playlist_id))
+    conn.commit()
+    conn.close()
+
 def get_playlist(id: str) -> tuple:
     conn: Connection = sqlite3.connect("saved_playlists.db", check_same_thread=False)
 
     sql = ''' SELECT * FROM test WHERE id = ? LIMIT 1'''
     cur = conn.cursor()
-    return cur.execute(sql, (id,)).fetchone()
+    fetched_data = cur.execute(sql, (id,)).fetchone()
+    
+    conn.close()
+    return fetched_data
 
 if __name__ == '__main__':
 
@@ -50,5 +63,5 @@ if __name__ == '__main__':
     conn: Connection = sqlite3.connect("saved_playlists.db", check_same_thread=False)
     create_table(conn, create_table_test)
     conn.commit()
-    insert_data((1029, "meow", "some title idk", None, None, None, None, None, None, None, None, None))
+    initialize_playlist((1029, "meow", "some title idk", None, None, None, None, None, None, None, None, None))
     
