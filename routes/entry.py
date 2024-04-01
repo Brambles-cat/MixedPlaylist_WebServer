ba3583b3f599439f8e63bd1243a852b189a8ecb2
Brambles-cat <gael.alejos05@gmail.com>
2024-04-01 14:46:40 -0700

    Clear session when visiting

--- routes/entry.py
+++ routes/entry.py
@@ -1,7 +1,8 @@
-from flask import redirect
+from flask import redirect, session
 from app import flaskapp, address
 
 # might do some stuff here later idk lol
 @flaskapp.route("/")
 def enter():
+	session.clear()
 	return redirect(f'{address}/create')
