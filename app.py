3cce75cc2454a4241b4af9d8c379d3043522a865
Brambles-cat <gael.alejos05@gmail.com>
2024-04-01 15:19:06 -0700

    smol thing

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import secrets
 import socket
 from flask import Flask, render_template
+import os
+import dotenv
 
+dotenv.load_dotenv()
 # different value depending on whether my laptop is being used or an RPi
-usingRPi = True
-address = f"https://www.mixedplaylist.com"
+usingRPi = os.getenv('usingRPi')
+address = "https://www.mixedplaylist.com" if usingRPi else f"http://{socket.gethostbyname(socket.gethostname())}"
 
 def render_create(**context):
     return render_template("create.html", address=address, **context)
