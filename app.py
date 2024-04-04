99a3bd209db6c927c7a3b72bbfe1fde46a953afa
Brambles-cat <gael.alejos05@gmail.com>
2024-04-04 09:08:50 -0700

    Light and Dark themes (Colors are WIP)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from flask import Flask, render_template, request, make_response, Response
 import secrets
 import socket
-from flask import Flask, render_template
+import uuid
 import os
 import dotenv
 
@@ -9,10 +10,18 @@ dotenv.load_dotenv()
 usingRPi = os.getenv('usingRPi')
 address = "https://www.mixedplaylist.com" if usingRPi else f"http://{socket.gethostbyname(socket.gethostname())}"
 
-def render_create(**context):
-    return render_template("create.html", address=address, **context)
 
 flaskapp = Flask(__name__)
 flaskapp.secret_key = secrets.token_urlsafe()
 
 flaskapp.config['SESSION_PERMANENT'] = False
+
+def render_create(**context):
+    return render_template("create.html", address=address, theme=request.cookies.get("theme") , **context)
+
+def ensure_cookies(resp: Response) -> bool:
+    if request.cookies.get("uid") is None:
+        resp.set_cookie("uid", str(uuid.uuid4()), max_age=None, path='/', secure=True, httponly=True)
+        resp.set_cookie("theme", "light", max_age=None, path='/', secure=True, httponly=True)
+        return True
+    return False
---
+++ routes/switch_theme.py
@@ -0,0 +1,18 @@
+from flask import make_response, request
+from app import flaskapp, ensure_cookies
+import uuid
+
+@flaskapp.route('/switch_theme', methods=['POST'])
+def switch_theme():
+    resp = make_response("{result: 'Success'}")
+    cookie_made = ensure_cookies(resp)
+
+    if cookie_made:
+        resp.set_cookie("theme", "dark", max_age=None, path='/', secure=True, httponly=True)
+    else:
+        resp.set_cookie(
+            "theme",
+            "dark" if request.cookies.get("theme") == "light" else "light",
+            max_age=None, path='/', secure=True, httponly=True)
+    
+    return resp
