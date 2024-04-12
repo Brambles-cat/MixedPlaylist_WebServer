907b229d9acf21d2de04802b828dc94539a23720
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2024-04-11 18:04:30 -0700

    dtfyugiuhuygyutfyd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from app import flaskapp
 from modules.routes import entry, create, playlist, switch_theme, validator
-import modules.bag_o_nifty_stuff as bag
 
 if __name__ == '__main__':
-	print(bag.API_KEY)
 	flaskapp.run(host='0.0.0.0', port=5000, debug=False)
